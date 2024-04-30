dd2b6c4c932ba5900bd12b0fab34d7870ca3ba39
Mauriciorpx <mauricio3741@gmail.com>
2024-04-29 22:27:54 -0500

    Importar funciones

--- function.php
+++ function.php
@@ -1,9 +1,14 @@
 <?php  
-    function saludo($nombre){
+    /* function saludo($nombre){
         return "Hola ".$nombre."<br>";
     }
 
     $saludo = saludo("David");
     echo $saludo;
+ */
+    function promedio_alumno($valor1, $valor2, $valor3){
+        $promedio = ($valor1+$valor2+$valor3)/3;
+        return $promedio;
+    }
     
     
---
+++ incluir_funciones.php
@@ -0,0 +1,4 @@
+<?php
+    include "function.php";
+
+    echo "El promedio es: ". promedio_alumno(5,6,7);
