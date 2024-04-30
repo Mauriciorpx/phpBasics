890fb7320b9cdcfee345b64e4cfdbbe2a80d9109
Mauriciorpx <mauricio3741@gmail.com>
2024-04-30 15:28:29 -0500

    Formularios segunda parte

--- formulario.php
+++ formulario.php
@@ -14,8 +14,21 @@
         
         <br>
 
-        <label for="asignatura">Asignatura</label>
+        <!-- Select unico -->
+        <!-- <label for="asignatura">Asignatura</label>
         <select name="asignatura" id="asignatura">
+            <option value="Ingles">Ingles</option>
+            <option value="Matematica">Matematica</option>
+            <option value="Ciencia">Ciencia</option>
+            <option value="Lenguaje">Lenguaje</option>
+        </select> -->
+
+        <br>
+
+        <!-- Select Multiple -->
+        <label for="asignatura">Asignatura</label>
+        <br>
+        <select name="asignatura[]" id="asignatura" Multiple>
             <option value="Ingles">Ingles</option>
             <option value="Matematica">Matematica</option>
             <option value="Ciencia">Ciencia</option>
@@ -23,14 +36,21 @@
         </select>
 
         <br><br>
-
-        <label for="opcion1">
-            <input type="checkbox" value="Manzana" id="opcion-1" name= "frutas">
+        <!-- El value es el valor que se manda al enviar el formulario -->
+        <label for="opcion-1">
+            <input type="checkbox" value="Manzana" id="opcion-1" name= "frutas[]">
             Manzana
         </label>
-
+        <label for="opcion-2">
+            <input type="checkbox" value="Pera" id="opcion-2" name= "frutas[]">
+            Pera
+        </label>
+        <label for="opcion-3">
+            <input type="checkbox" value="Fresa" id="opcion-3" name= "frutas[]">
+            Fresa
+        </label>
         <br><br><br>
-        
+
         <button type="submit">Enviar</button>
         
 
--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
     //Se capturan los datos del input mediante el identificador "name"
-   $nombre = $_POST['nombre'];
+    //Se usa el metodo post, ya que este no envia los datos a la URL, mientras que GET si lo hace
+   /* $nombre = $_POST['nombre'];
    $asignatura = $_POST['asignatura'];
-   $frutas = $_POST['frutas'];
+   $frutas = $_POST['frutas']; */
 
-   echo $nombre."-".$asignatura."-".$frutas;
+   /* echo $nombre."-".$asignatura."-".$frutas; */
+    $materias = $_POST['asignatura'];
+   foreach($materias as $materia ){
+    echo $materia."<br>";
+   }
+
+   echo "<br>";
+   echo "<br>";
+   echo "<br>";
+   
+   $fresa = $_POST['frutas'];
+
+   foreach($fresa as $frutas ){
+    echo $frutas."<br>";
+   }
