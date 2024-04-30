e52c2385961b520d406b524fec66e11105d45dde
Mauriciorpx <mauricio3741@gmail.com>
2024-04-29 21:26:43 -0500

    include

---
+++ include.php
@@ -0,0 +1,10 @@
+<?php
+    include("for.php");
+   // include "foreach.php";
+   require "break_continue.php";
+   require "break_continue.php";
+
+   //require_one: en caso de llamar 2 veces al mismo archivo, solo se ejecuta 1 vez
+
+   include_once("match.php");
+   include_once("match.php");
